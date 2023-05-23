1acc23e69e8e6924ee0756a0856f49e901001d00
Harinarayanan K P <harikpaug18@gmail.com>
2023-05-23 20:23:11 +0530

    Third Commit

--- src/components/mediabox/Mediabox.jsx
+++ src/components/mediabox/Mediabox.jsx
@@ -8,23 +8,26 @@ import './mediabox.css'
 function Mediabox() {
     return <ul class="mediabox">
         <li>
-            <a className='facebook' target="_blank" rel="noreferrer" href="https://www.facebook.com/harinarayanan.kp.58">
-                <div><img className='icon' src={facebook} alt=""></img></div>
-            </a>
-            <a className='linkedin' target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/harinarayanan-kp-a25094255/">
-                <div><img className='icon' src={linkedin} alt=""></img></div>
-            </a>
-            <a className='twitter' target="_blank" rel="noreferrer" href="https://twitter.com/Harinarayanan08">
-                <div><img className='icon' src={twitter} alt=""></img></div>
-            </a>
-            <a className='instagram' target="_blank" rel="noreferrer" href="https://www.instagram.com/harinarayanan.kp/">
-                <div><img className='icon' src={instagram} alt=""></img></div>
-            </a>
-            <a className='gmail' target="_blank" rel="noreferrer" href="mailto:harikpaug18@gmail.com">
-                <div>
+            <div>
+                <a className='facebook' target="_blank" rel="noreferrer" href="https://www.facebook.com/harinarayanan.kp.58">
+                    <img className='icon' src={facebook} alt=""></img>
+                </a>
+            </div>
+            <div>
+                <a className='linkedin' target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/harinarayanan-kp-a25094255/">
+                    <img className='icon' src={linkedin} alt=""></img>
+                </a></div><div>
+                <a className='twitter' target="_blank" rel="noreferrer" href="https://twitter.com/Harinarayanan08">
+                    <img className='icon' src={twitter} alt=""></img>
+                </a></div><div>
+                <a className='instagram' target="_blank" rel="noreferrer" href="https://www.instagram.com/harinarayanan.kp/">
+                    <img className='icon' src={instagram} alt=""></img>
+                </a></div><div>
+                <a className='gmail' target="_blank" rel="noreferrer" href="mailto:harikpaug18@gmail.com">
+
                     <img className='icon' src={gmail} alt=""></img>
-                </div>
-            </a>
+
+                </a></div>
         </li>
     </ul>
 }
